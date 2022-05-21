f559be91e45755647ad87955f0485725ea3e3d23
cagatayyildiz <yildiz.cagatay@hotmail.com>
2022-05-21 13:03:43 +0300

    swift get request

--- PlantLiveApp/models/Category.swift
+++ PlantLiveApp/models/Category.swift
@@ -3,11 +3,12 @@
 //  PlantLiveApp
 //
 //  Created by Çağatay Yıldız on 15.05.2022.
-//
+
 
 import Foundation
 
-struct Category{
+struct Category :  Codable, Hashable {
+    var id : Int = 0
     var name : String = ""
     var description : String = ""
 }
---
+++ PlantLiveApp/views/screens/CategoryList.swift
@@ -0,0 +1,51 @@
+//
+//  CategoryList.swift
+//  PlantLiveApp
+//
+//  Created by Çağatay Yıldız on 21.05.2022.
+//
+
+import SwiftUI
+
+struct CategoryList: View {
+    
+    @State var categories : [Category] = []
+    
+    
+    var body: some View {
+        
+        VStack{
+            ForEach(categories, id:\.self){item in
+                
+                Text(item.name)
+            }
+
+        }.onAppear(){
+            
+            let url = URL(string: "https://northwind.vercel.app/api/categories")!
+            
+            URLSession.shared.dataTask(with: url){(data, response, error) in
+                
+                do{
+                    
+                    if let categoryList = data{
+                        
+                        let decodeData = try JSONDecoder().decode([Category].self , from: categoryList)
+                        
+                        categories = decodeData
+                        
+//                        DispatchQueue.main.async {
+//                            categories = decodeData
+//                        }
+                     
+                    }
+                }
+                catch{
+                    print("Category Error!!")
+                }
+            }.resume()
+            
+            
+        }
+    }
+}
