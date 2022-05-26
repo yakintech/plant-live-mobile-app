0b5b77916b25a9badfa0055af479adc21bcc9cf2
Berkaysln <berkaysln1@gmail.com>
2022-05-26 17:53:46 +0300

    Created NavigationLink for ProfilePlantList

--- PlantLiveApp/views/screens/Profile/ProfilePlantList.swift
+++ PlantLiveApp/views/screens/Profile/ProfilePlantList.swift
@@ -22,19 +22,24 @@ struct ProfilePlantList: View {
       
       ScrollView(showsIndicators: false) {
         Spacer()
+        
         LazyVGrid(columns: columns, spacing: 10) {
           ForEach(plants, id:\.self) { item in
-            VStack {
-              Image(item)
-                .resizable()
-                .frame(width: 160, height: 160)
-                .background(RoundedRectangle(cornerRadius: 20)
-                  .stroke(LinearGradient(colors: [Color("defaultgreen"), Color("olivine")], startPoint: .bottomTrailing, endPoint: .topLeading), lineWidth: 5))
-              Text(item)
-                .frame(width: 120, height: 30)
-                .background(LinearGradient(colors: [Color("defaultgreen"), Color("olivine")], startPoint: .bottom, endPoint: .top))
-                .cornerRadius(20)
-                .foregroundColor(.white)
+            
+            NavigationLink(destination: PlantDetail(name: item, description: item.description)){
+              
+              VStack {
+                Image(item)
+                  .resizable()
+                  .frame(width: 160, height: 160)
+                  .background(RoundedRectangle(cornerRadius: 20)
+                    .stroke(LinearGradient(colors: [Color("defaultgreen"), Color("olivine")], startPoint: .bottomTrailing, endPoint: .topLeading), lineWidth: 5))
+                Text(item)
+                  .frame(width: 120, height: 30)
+                  .background(LinearGradient(colors: [Color("defaultgreen"), Color("olivine")], startPoint: .bottom, endPoint: .top))
+                  .cornerRadius(20)
+                  .foregroundColor(.white)
+              }
             }
           }
         }
