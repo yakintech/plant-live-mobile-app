ac99f81e822adb15188198482a7c274fa7c2eab9
zelihauslu <zelusluz@gmail.com>
2022-06-04 22:49:32 +0300

    tipe model edited

--- PlantLiveApp/views/screens/CategoryDetail/CategoryDetailView.swift
+++ PlantLiveApp/views/screens/CategoryDetail/CategoryDetailView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct CategoryDetailView: View {
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     @State private var dongu : [String] = [
     "hello1","hello2","hello3","hello4","hello5","hello6","hello7","hello8","hello9","hello10"
     ]
     var body: some View {
+        
         NavigationView{
-            
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(dongu, id:\.self){item in
+                    Text("\(item)")
+                }
+            }
         }
     }
 }
