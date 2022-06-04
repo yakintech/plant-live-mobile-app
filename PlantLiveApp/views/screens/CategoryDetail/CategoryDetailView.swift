274b143fe5d3762684fffd888b1429f3108a602c
zelihauslu <zelusluz@gmail.com>
2022-06-04 22:37:37 +0300

    alamofire package has been re-added

---
+++ PlantLiveApp/views/screens/CategoryDetail/CategoryDetailView.swift
@@ -0,0 +1,25 @@
+//
+//  CategoryDetailView.swift
+//  PlantLiveApp
+//
+//  Created by Zeliha Uslu on 4.06.2022.
+//
+
+import SwiftUI
+
+struct CategoryDetailView: View {
+    @State private var dongu : [String] = [
+    "hello1","hello2","hello3","hello4","hello5","hello6","hello7","hello8","hello9","hello10"
+    ]
+    var body: some View {
+        NavigationView{
+            
+        }
+    }
+}
+
+struct CategoryDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryDetailView()
+    }
+}
