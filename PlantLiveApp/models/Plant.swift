fdcba5550841425963b04c0fde0c6d5fcf7c222e
Emiralp <emiralpduman@gmail.com>
2022-06-10 22:35:54 +0300

    Plant struct's id propety is renamed to _id

--- PlantLiveApp/models/Plant.swift
+++ PlantLiveApp/models/Plant.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct Plant : Hashable, Decodable {
     
-    var id: String = "629c81407cf041db9ca231d0"
+    var _id: String = "629c81407cf041db9ca231d0"
     var name: String = "Plant's name"
     var description: String = "Ofis ve ev ortamlarında rahat bir şekilde yetiştirilecek yemyeşil bir bitki olan bu çiçek; uzun ömürlü bir çiçek olmakla beraber çok dayanıklıdır. Güzel ve geniş yaprakları bulunur. Deve tabanı çiçeğinin bakımı çok basittir. Hava kalitesini de olumlu bir şekilde arttırdığından ofis ve evlerde çok sık tercih edilir. Yapraklarının şekli büyük kalp şeklinde olup parçalı yapraklara sahiptir."
     var img: String = "devetabani"
