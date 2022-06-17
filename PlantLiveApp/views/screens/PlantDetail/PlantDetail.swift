f206217cfa69636cc42d30dcdc317d6c4e413c3b
Emiralp <emiralpduman@gmail.com>
2022-06-17 22:04:45 +0300

    PlantDetail'in farklı cihazlarda da iyi gözükmesi için GeometryReader ve .frame uygulaması

--- PlantLiveApp/views/screens/PlantDetail/PlantDetail.swift
+++ PlantLiveApp/views/screens/PlantDetail/PlantDetail.swift
@@ -15,42 +15,43 @@ struct PlantDetail: View {
     @ObservedObject private var plant: PlantVM
     @State var plantDetailOthersImage = false
     var body: some View {
-        VStack {
-            
+        GeometryReader { geometry in
             VStack {
-                MapView(coordinates: plant.coordinates)
-                    .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-                Button(action: {
-                    self.plantDetailOthersImage.toggle()
-                }, label: {
-                    CircleImage(imageName: plant.imageName)
-                }).offset(y: -130)
-                    .padding(.bottom, -130)
-                    .sheet(isPresented: self.$plantDetailOthersImage, content: {PlantDetailBigImage(imageName: plant.imageName)})
-                
-            }
-
-            VStack(alignment: .leading) {
-                Text(plant.name)
-                    .font(.title)
-                HStack {
-                    Text(plant.countryOfOrigin)
-                    Spacer()
-
+                VStack {
+                    MapView(coordinates: plant.coordinates)
+                        .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+                    Button(action: {
+                        self.plantDetailOthersImage.toggle()
+                    }, label: {
+                        CircleImage(imageName: plant.imageName)
+                    }).offset(y: -130)
+                        .padding(.bottom, -130)
+                        .sheet(isPresented: self.$plantDetailOthersImage, content: {PlantDetailBigImage(imageName: plant.imageName)})
+                    
                 }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
+                .frame(height: geometry.size.height / 2)
 
-                Divider()
-                Text("About \(plant.name)")
-                    .font(.title2)
-                Text(plant.description)
+                VStack(alignment: .leading) {
+                    Text(plant.name)
+                        .font(.title)
+                    HStack {
+                        Text(plant.countryOfOrigin)
+                        Spacer()
+
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+
+                    Divider()
+                    Text("About \(plant.name)")
+                        .font(.title2)
+                    Text(plant.description)
+                }
+                .padding()
+                Spacer()
             }
-            .padding()
-            Spacer()
         }
-            
     }
 }
 
