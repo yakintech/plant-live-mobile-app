ea0ca3fe5de086f71c666353d59ab19f89926ac6
erenyurekli97 <105647103+erenyurekli97@users.noreply.github.com>
2022-06-26 11:19:44 +0300

    created common folder.

---
+++ PlantLiveApp/Common/Constants/ApiConfig.swift
@@ -0,0 +1,16 @@
+//
+//  ApiConfig.swift
+//  PlantLiveApp
+//
+//  Created by Çağatay Yıldız on 19.06.2022.
+//
+
+import Foundation
+
+class ApiConfig
+{
+    static let baseUrl : String = "https://plankton-app-jr8ee.ondigitalocean.app"
+}
+
+
+
