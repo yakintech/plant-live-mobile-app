2f94c5b92dbb12aab45206edba00f9e276548bc2
Berkaysln <berkaysln1@gmail.com>
2022-06-27 18:01:29 +0300

    fix white space at top in Simulator

--- PlantLiveApp/views/screens/Profile/ProfilePage.swift
+++ PlantLiveApp/views/screens/Profile/ProfilePage.swift
@@ -3,55 +3,42 @@ import SwiftUI
 struct ProfilePage: View {
   
   @State private var fullName = "Jack Harlow"
-  @State private var isPressed = false
   
   var body: some View {
-
-      ZStack {
-          
+    
+    VStack {
+      HStack {
+        Image("jackHarlow")
+          .resizable()
+          .frame(width: 140, height: 140)
+          .clipShape(Circle())
+          .padding(.trailing, 50.0)
         VStack {
-          HStack {
-            Image("jackHarlow")
-              .resizable()
-              .frame(width: 140, height: 140)
-              .clipShape(Circle())
-              .padding()
-            VStack {
-              Text(fullName)
-                .font(.headline)
-                .fontWeight(.semibold)
-              NavigationLink(destination: EditProfile()) {
-                Text("Edit Profile")
-                  .frame(width: 110, height: 30)
-                  .foregroundColor(.white)
-                  .background(Color("defaultgreen"))
-                  .cornerRadius(12)
-              }
-            }
+          Text(fullName)
+            .font(.headline)
+            .fontWeight(.semibold)
+          NavigationLink(destination: EditProfile()) {
+            Text("Edit Profile")
+              .frame(width: 110, height: 30)
+              .foregroundColor(.white)
+              .background(Color("defaultgreen"))
+              .cornerRadius(12)
           }
-          VStack {
-            HStack {
-              Text("My Garden")
-                .font(.body)
-                .fontWeight(.black)
-                .foregroundColor(Color("defaultgreen"))
-                .imageScale(.large)
-              
-              Spacer()
-              
-              Button(action: {
-                isPressed.toggle()
-              }) {
-                Image(systemName: isPressed ? "star.fill" : "star")
-                  .foregroundColor(Color("defaultgreen"))
-                  .imageScale(.large)
-              }
-            }.padding(.horizontal)
-          }
-          ProfilePlantList()
         }
       }
-      
+      VStack {
+        HStack {
+          Text("My Garden")
+            .font(.body)
+            .fontWeight(.black)
+            .foregroundColor(Color("defaultgreen"))
+            .imageScale(.large)
+          
+        }.padding(.horizontal)
+      }
+      ProfilePlantList()
+        .navigationBarHidden(true)
+    }
   }
 }
 
